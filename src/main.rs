a237956c9e15180ed7a89205e17982ec3beb86d5
cloudgazing <cloudmiles00@gmail.com>
2024-08-02 02:29:07 +0300

    Changed to encrypted WSS connection

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,40 @@
-use chrono::{Duration, Utc};
 use futures_util::sink::SinkExt;
 use futures_util::stream::StreamExt;
-use jwt_compact::{
-	alg::{Hs256, Hs256Key},
-	prelude::*,
-};
-use tokio::net::TcpListener;
+use jwt_compact::alg::Hs256Key;
+use rustls::pki_types::{CertificateDer, PrivateKeyDer};
+use rustls_pemfile::{certs, pkcs8_private_keys};
+use std::sync::Arc;
+use tokio::net::{TcpListener, TcpStream};
+use tokio_rustls::{server::TlsStream, TlsAcceptor};
 use tokio_tungstenite::accept_async;
 use tokio_tungstenite::tungstenite::protocol::Message;
 
 mod models;
-use models::{ClientMessage, TokenClaims};
+use models::{ClientMessage, LoginConfirmation, ServerMessage, TokenClaims};
 
 fn generate_jwt(key: &Hs256Key, user_id: String) -> Result<String, jwt_compact::CreationError> {
-	let custom_claims = TokenClaims { user_id };
+	use jwt_compact::{alg::Hs256, AlgorithmExt, Claims, Header, TimeOptions};
 
 	let time_options = TimeOptions::default();
+
+	let custom_claims = TokenClaims { user_id };
 
 	let header = Header::empty().with_key_id("my-key");
 
 	let claims = Claims::new(custom_claims)
-		.set_duration_and_issuance(&time_options, Duration::days(10))
-		.set_not_before(Utc::now());
+		.set_duration_and_issuance(&time_options, chrono::Duration::days(10))
+		.set_not_before(chrono::Utc::now());
 
 	Hs256.token(&header, &claims, &key)
 }
 
-fn get_jwt(token_string: String, verifying_key: &Hs256Key) -> Result<Token<TokenClaims>, anyhow::Error> {
-	let token = UntrustedToken::new(&token_string)?;
-
-	let token: Token<TokenClaims> = Hs256.validator(verifying_key).validate(&token)?;
+fn get_jwt(token_string: String, verifying_key: &Hs256Key) -> Result<jwt_compact::Token<TokenClaims>, anyhow::Error> {
+	use jwt_compact::{alg::Hs256, prelude::UntrustedToken, AlgorithmExt, TimeOptions};
 
 	let time_options = TimeOptions::default();
+
+	let token = UntrustedToken::new(&token_string)?;
+	let token = Hs256.validator(verifying_key).validate(&token)?;
 
 	token
 		.claims()
@@ -41,20 +44,68 @@ fn get_jwt(token_string: String, verifying_key: &Hs256Key) -> Result<Token<Token
 	Ok::<_, anyhow::Error>(token)
 }
 
-async fn handle_connection(stream: tokio::net::TcpStream) {
+async fn handle_connection(stream: TlsStream<TcpStream>) {
 	let ws_stream = accept_async(stream).await.expect("Error during WebSocket handshake");
 
 	println!("New WebSocket connection");
 
-	let (mut ws_sender, mut ws_receiver) = ws_stream.split();
+	let (mut sender, mut receiver) = ws_stream.split();
 
-	while let Some(message) = ws_receiver.next().await {
+	while let Some(message) = receiver.next().await {
+		let message = match message {
+			Ok(message) => message,
+			Err(e) => {
+				println!("WebSocket error: {e}");
+				continue;
+			}
+		};
+
 		match message {
-			Ok(Message::Text(text)) => {
-				match serde_json::from_str::<ClientMessage>(&text) {
-					Ok(ClientMessage::LoginData(data)) => {
-						// validate login data
-						println!("Received login data: {:?}", data);
+			Message::Text(text) => {
+				let client_message = match serde_json::from_str::<ClientMessage>(&text) {
+					Ok(message) => message,
+					Err(e) => {
+						println!("Error: {e}");
+						continue;
+					}
+				};
+
+				match client_message {
+					ClientMessage::LoginData(_data) => {
+						//TODO: validate login data
+
+						//TODO: get user_id from db
+						let user_id = "test-user-id".to_string();
+
+						let key = Hs256Key::new(b"super_secret_key_donut_steel");
+
+						let token_string = generate_jwt(&key, user_id);
+
+						match token_string {
+							Ok(access_token) => {
+								let login_confirmation = LoginConfirmation { access_token };
+
+								let server_message = ServerMessage::LoginConfirmation(login_confirmation);
+
+								let message_string = serde_json::to_string(&server_message).unwrap();
+
+								sender
+									.send(Message::Text(message_string))
+									.await
+									.expect("Failed to send auth token");
+							}
+							Err(e) => {
+								println!("Error: {e}");
+								sender
+									.send(Message::Text("There was an error loging you in".to_string()))
+									.await
+									.expect("Failed to send login error");
+							}
+						}
+					}
+					ClientMessage::SignupData(data) => {
+						// validate signup data
+						println!("Received signup data: {:?}", data);
 
 						// get user_id
 						let user_id = "test-user-id".to_string();
@@ -65,21 +116,21 @@ async fn handle_connection(stream: tokio::net::TcpStream) {
 
 						match token_string {
 							Ok(token_string) => {
-								ws_sender
+								sender
 									.send(Message::Text(token_string))
 									.await
 									.expect("Failed to send auth token");
 							}
 							Err(e) => {
 								println!("Error: {e}");
-								ws_sender
-									.send(Message::Text("There was an error loging you in".to_string()))
+								sender
+									.send(Message::Text("There was an error signing up".to_string()))
 									.await
-									.expect("Failed to send auth token");
+									.expect("Failed to send signup error");
 							}
 						}
 					}
-					Ok(ClientMessage::SentMessage(data)) => {
+					ClientMessage::SentMessage(data) => {
 						let key = Hs256Key::new(b"super_secret_key_donut_steel");
 						let token = get_jwt(data.access_token, &key);
 
@@ -94,16 +145,10 @@ async fn handle_connection(stream: tokio::net::TcpStream) {
 							}
 						}
 					}
-					Err(e) => {
-						println!("Error: {e}");
-					}
 				}
 			}
-			Ok(Message::Close(_)) => {
+			Message::Close(_) => {
 				println!("Client disconnected");
-			}
-			Err(e) => {
-				println!("Error: {:?}", e);
 			}
 			_ => (),
 		}
@@ -112,12 +157,44 @@ async fn handle_connection(stream: tokio::net::TcpStream) {
 
 #[tokio::main]
 async fn main() {
-	let addr = "127.0.0.1:3030";
-	let listener = TcpListener::bind(addr).await.expect("Failed to bind");
+	// used for testing
+	let address = "192.168.86.250:8080";
 
-	println!("WebSocket server running on ws://{}", addr);
+	let cert_path = "./cert/fullchain.pem";
+	let key_path = "./cert/privkey.pem";
+
+	let file = std::fs::File::open(cert_path).expect("Cert file error");
+	let mut reader = std::io::BufReader::new(file);
+	let cert_chain: std::io::Result<Vec<CertificateDer<'static>>> = certs(&mut reader).collect();
+	let cert_chain = cert_chain.unwrap();
+
+	let file = std::fs::File::open(key_path).expect("Key file error");
+	let mut reader = std::io::BufReader::new(file);
+	let key_der: std::io::Result<PrivateKeyDer<'static>> =
+		pkcs8_private_keys(&mut reader).next().unwrap().map(Into::into);
+	let key_der = key_der.unwrap();
+
+	let config = rustls::ServerConfig::builder()
+		.with_no_client_auth()
+		.with_single_cert(cert_chain, key_der)
+		.expect("Error creating server config");
+
+	let acceptor = TlsAcceptor::from(Arc::new(config));
+	let listener = TcpListener::bind(address).await.expect("Failed to bind address");
+
+	println!("WebSocket server running on {}", address);
 
 	while let Ok((stream, _)) = listener.accept().await {
-		tokio::spawn(handle_connection(stream));
+		let acceptor = acceptor.clone();
+
+		let tls_stream = match acceptor.accept(stream).await {
+			Ok(stream) => stream,
+			Err(e) => {
+				eprintln!("Failed to establish TLS: {:?}", e);
+				continue;
+			}
+		};
+
+		tokio::spawn(handle_connection(tls_stream));
 	}
 }
--- src/models/mod.rs
+++ src/models/mod.rs
@@ -12,6 +12,13 @@ pub struct LoginData {
 }
 
 #[derive(Serialize, Deserialize, Debug)]
+pub struct SignupData {
+	pub access_key: String,
+	pub password: String,
+	pub display_name: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
 pub struct SentMessage {
 	pub access_token: String,
 	pub user_id: String,
@@ -22,6 +29,7 @@ pub struct SentMessage {
 #[derive(Serialize, Deserialize)]
 pub enum ClientMessage {
 	LoginData(LoginData),
+	SignupData(SignupData),
 	SentMessage(SentMessage),
 }
 
@@ -31,7 +39,19 @@ pub struct NewMessage {
 	pub content: String,
 }
 
+#[derive(Serialize, Deserialize, Debug)]
+pub struct LoginConfirmation {
+	pub access_token: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct SignupConfirmation {
+	pub access_token: String,
+}
+
 #[derive(Serialize, Deserialize)]
 pub enum ServerMessage {
 	NewMessage(NewMessage),
+	LoginConfirmation(LoginConfirmation),
+	SignupConfirmation(SignupConfirmation),
 }
